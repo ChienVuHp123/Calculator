c33ef395c69d98e15787b7f64ea92c760ed4279a
ChienVuHp123 <57214915+ChienVuHp123@users.noreply.github.com>
2022-05-10 00:15:42 +0700

    Update MainActivity.kt

--- app/src/main/java/code/Android/Studio/MyCalculator/MainActivity.kt
+++ app/src/main/java/code/Android/Studio/MyCalculator/MainActivity.kt
@@ -8,66 +8,139 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity()
 {
-    private var canAddOperation = false
-    private var canAddNumber = true
+    private var canAddOperation = false                   // Bien kiem tra co the them dau toan hoc khong
+    private var canAddNumber = true                       // Bien kiem tra co the them so duoc khong
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    fun numberAction(view: View)
+    fun numberAction(view: View)      // ham them ki tu so 
     {
-        if(view is Button)
+        if(view is Button)    // neu che do xem la button 
         {
             if(view.text == ".")
             {
                 if(canAddNumber)
-                    workingsTV.append(view.text)
-                canAddNumber = false
+                    workingsTV.append(view.text)   // them ki tu tu bat ki nut nao
+                canAddNumber = false               // de bien nay ve false the hien rang khong the them ki tu laf so nua
             }
             else
                 workingsTV.append(view.text)
-            canAddOperation = true
+            canAddOperation = true                // co the them ki tu la ki tu toan hoc
         }
     }
-    fun operationAction(view: View)
+    fun operationAction(view: View)    // ham them ki tu toan hoc
     {
-        if(view is Button && canAddOperation)
+        if(view is Button && canAddOperation)     // neu view la button va dang co the them ki tu toan hoc
         {
-            workingsTV.append(view.text)
-            canAddOperation = false
-            canAddNumber = true
+            workingsTV.append(view.text)         // them ki tu toan hoc vao string workingstv
+            canAddOperation = false              // sau khi them ki tu toan hoc thi chuyen canAddOperation de khong cho them ki tu toan hoc nua
+            canAddNumber = true                  // chuyen thanh true de co the them ki tu so
         }
     }
+    
+    /* 
+    Ham de xoa tat ca phep tinh va ket qua bang cach cai dat chuoi workingtextview va resulttv bang null
+    */
     fun allClearAction(view: View)
     {
         workingsTV.text = ""
         resultsTV.text = ""
     }
+    
     fun backSpaceAction(view: View)
     {
-        val length = workingsTV.length()
+        val length = workingsTV.length()    // do dai chuoi working tv
         if(length > 0)
-            workingsTV.text = workingsTV.text.subSequence(0, length - 1)
+            workingsTV.text = workingsTV.text.subSequence(0, length - 1) // xoa 1 ki tu cuoi cung
     }
-    fun equalsAction(view: View)
+    
+     private fun digitsOperators(): MutableList<Any>    // ham chuyen cac ki tu so va toan hoc vào mảng list
+    {
+        val list = mutableListOf<Any>()    // tao bien danh sach 
+        var currentDigit = ""            // chuoi so hien tai
+        for(character in workingsTV.text)  // ki tu o trong chuoi workingsTV
+        {
+            if(character.isDigit() || character == '.') // neu ki tu la chuoi so hoac la ki tu thap phan
+                currentDigit += character               // them ki tu hien tai vao chuoi currentDigit  
+            else                                        // neu khong phai 
+            {
+                list.add(currentDigit.toFloat())       // them chuoi currentDigit chuyen sang kieu float vao list
+                currentDigit = ""                      // chuyen bien current ve null
+                list.add(character)                    // them ki tu toan hoc vao list
+            }
+        }
+        if(currentDigit != "")                         // de them nhung ki tu con lai vao list
+            list.add(currentDigit.toFloat())
+        return list
+    }
+    
+    fun equalsAction(view: View)    // ham tinh ket qua
     {
         resultsTV.text = calculateResults()
     }
+    
     private fun calculateResults(): String
     {
         val digitsOperators = digitsOperators()
-        if(digitsOperators.isEmpty())
+        if(digitsOperators.isEmpty())     // neu string digitsO ma trong thi tra ve null
             return ""
 
-        val timesDivision = timesDivisionCalculate(digitsOperators)
+        val timesDivision = timesDivisionCalculate(digitsOperators)   // tinh phep tinh nhan, chia
         if(timesDivision.isEmpty())
             return ""
 
-        val result = addSubtractCalculate(timesDivision)
+        val result = addSubtractCalculate(timesDivision)        // tinh phep tinh cong tru
         return result.toString()
     }
-    private fun addSubtractCalculate(passedList: MutableList<Any>): Float
+    
+     private fun timesDivisionCalculate(passedList: MutableList<Any>): MutableList<Any>
+    {
+        var list = passedList
+        while (list.contains('x') || list.contains('/'))  // neu trong passlist ma co ki nhan hoac chia thi them vao 
+        {
+            list = calcTimesDiv(list)                    // thuc hien phep tinh nhan, chia tra ve list
+        }
+        return list
+    }
+     private fun calcTimesDiv(passedList: MutableList<Any>): MutableList<Any>   // ham thuc hien phep tinh nhan. chia
+    {
+        val newList = mutableListOf<Any>()
+        var restartIndex = passedList.size  
+        for(i in passedList.indices)      // vong for nhay vi tri hop le cua list
+        {
+            if(passedList[i] is Char && i != passedList.lastIndex && i < restartIndex) // neu phan tu passedlist la 1 ki tu va i chua pai la phan tu cuoi cung va < rsindex
+            {
+                val operator = passedList[i]                    
+                val prevDigit = passedList[i - 1] as Float
+                val nextDigit = passedList[i + 1] as Float
+                when(operator)
+                {
+                    'x' ->
+                    {
+                        newList.add(prevDigit * nextDigit)
+                        restartIndex = i + 1
+                    }
+                    '/' ->
+                    {
+                        newList.add(prevDigit / nextDigit)
+                        restartIndex = i + 1
+                    }
+                    else ->                                // neu khong phai phep tinh nhan chia thi de nguyen va tra ve list
+                    {
+                        newList.add(prevDigit)
+                        newList.add(operator)
+                    }
+                }
+            }
+            if(i > restartIndex)
+                newList.add(passedList[i])
+        }
+        return newList
+    }
+    
+    private fun addSubtractCalculate(passedList: MutableList<Any>): Float   // ham thuc hien phep tinh cong, tru
     {
         var result = passedList[0] as Float
 
@@ -85,69 +158,7 @@ class MainActivity : AppCompatActivity()
         }
         return result
     }
-    private fun timesDivisionCalculate(passedList: MutableList<Any>): MutableList<Any>
-    {
-        var list = passedList
-        while (list.contains('x') || list.contains('/'))
-        {
-            list = calcTimesDiv(list)
-        }
-        return list
-    }
-    private fun digitsOperators(): MutableList<Any>
-    {
-        val list = mutableListOf<Any>()
-        var currentDigit = ""
-        for(character in workingsTV.text)
-        {
-            if(character.isDigit() || character == '.')
-                currentDigit += character
-            else
-            {
-                list.add(currentDigit.toFloat())
-                currentDigit = ""
-                list.add(character)
-            }
-        }
-        if(currentDigit != "")
-            list.add(currentDigit.toFloat())
-        return list
-    }
-    private fun calcTimesDiv(passedList: MutableList<Any>): MutableList<Any>
-    {
-        val newList = mutableListOf<Any>()
-        var restartIndex = passedList.size
-        for(i in passedList.indices)
-        {
-            if(passedList[i] is Char && i != passedList.lastIndex && i < restartIndex)
-            {
-                val operator = passedList[i]
-                val prevDigit = passedList[i - 1] as Float
-                val nextDigit = passedList[i + 1] as Float
-                when(operator)
-                {
-                    'x' ->
-                    {
-                        newList.add(prevDigit * nextDigit)
-                        restartIndex = i + 1
-                    }
-                    '/' ->
-                    {
-                        newList.add(prevDigit / nextDigit)
-                        restartIndex = i + 1
-                    }
-                    else ->
-                    {
-                        newList.add(prevDigit)
-                        newList.add(operator)
-                    }
-                }
-            }
-            if(i > restartIndex)
-                newList.add(passedList[i])
-        }
-        return newList
-    }
+   
 }
 
 
